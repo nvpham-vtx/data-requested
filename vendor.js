188de2ec36c6317c78fd4959be875c6f796cc8da
Phạm Ngọc Vinh <vinhngocphamhp@gmail.com>
2021-07-23 11:10:51 +0700

    Update vendor.js

--- vendor.js
+++ vendor.js
@@ -2,10 +2,4 @@
   subStr = function(str) {
     return str;
   }
- 
-  if (typeof define === 'function' && define.amd) {
-    define('vinhpn', [], function() {
-      return _;
-    });
-  }
 }.call(this));
